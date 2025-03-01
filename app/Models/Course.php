818bc83f3a04538e67b932894c442f2b6b327583
nas2dev <nas2dev@gmail.com>
2025-03-01 15:47:39 +0100

    CourseModule, Sections, Materials, UserCourseSectionProgress & UserCourseProgress modules added

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\CourseModule;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
@@ -25,5 +26,9 @@ class Course extends Model
 
     public function updatedBy() {
         return $this->belongsTo(User::class, 'updated_by')->selectUserName();
+    }
+
+    public function modules() {
+        return $this->hasMany(CourseModule::class, 'course_id');
     }
 }
