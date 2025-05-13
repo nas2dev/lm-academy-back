0725b047726b16123473f4aeb0c6f897a95c3c70
nas2dev <nas2dev@gmail.com>
2025-05-13 21:23:28 +0200

    Enhance AuthController to provide detailed error messages on login failure and return user roles in user profile and token response.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Unauthorized', 'message' => "Your email or password is invalid"], 401);
         }
 
         return $this->respondWithToken($token);
@@ -82,7 +82,14 @@ class AuthController extends Controller
      */
     public function userProfile()
     {
-        return response()->json(auth()->user()); 
+        $user_id = auth()->id();
+
+        $user = User::whereId($user_id)->with("roles")->first();
+
+        return response()->json([
+            "user" => $user
+        ]);
+        // return response()->json(auth()->user()); 
     }
 
     /**
@@ -141,11 +148,16 @@ class AuthController extends Controller
      */
     protected function respondWithToken($token)
     {
+
+        $user_id = auth()->id();
+
+        $user = User::whereId($user_id)->with("roles")->first();
+
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => $user
         ]);
     }
 
